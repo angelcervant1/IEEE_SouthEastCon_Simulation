34ae1c076d45709f831a86150c64aefd492bf6ab
angelcervant1 <angelcervant@hotmail.com>
2024-12-30 15:09:09 -0600

    modified run.sh

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 docker run -it --rm --gpus all --network host --privileged \
     -v /dev:/dev \
-    -v ~/ieee_simulation:/ieee_simulation \
+    -v ~/IEEE_SouthEastCon_Simulation:/IEEE_SouthEastCon_Simulation\
     -e DISPLAY=$DISPLAY \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
     ros-humble-cuda
