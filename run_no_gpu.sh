cf4ff657c04a958827cdc771451162d59484204a
angelcervant1 <angelcervant@hotmail.com>
2025-01-06 13:38:03 -0600

    created run for non nvidia users

---
+++ run_no_gpu.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+docker run -it --rm  --network host --privileged \
+    -v /dev:/dev \
+    -v ~/IEEE_SouthEastCon_Simulation:/IEEE_SouthEastCon_Simulation\
+    -e DISPLAY=$DISPLAY \
+    -v /tmp/.X11-unix:/tmp/.X11-unix \
+    ros-humble-cuda
